f97aff9c32719a4826771c6d07c3f0dc94674265
rds80 <rippleshah@msn.com>
2018-09-27 19:07:57 -0400

    Example of services created for each instance

---
+++ dependency-injection-ex100/src/app/car.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+
+export class CarService {
+
+  constructor() {
+      console.log('CarService: constructor')
+   }
+   //some dummy method.
+   isSuperCharged(car: string) {
+     return car === 'Ford GT' ? 'yes' : 'no'
+   }
+}
