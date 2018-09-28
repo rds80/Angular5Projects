3937e6964552bedd0955bd5d1460ffaa65d0025b
rds80 <rippleshah@msn.com>
2018-09-27 21:43:13 -0400

    Factory provider example

---
+++ dependency-injection-ex500/src/app/card.ts
@@ -0,0 +1,11 @@
+import {Injectable} from '@angular/core';
+
+@Injectable()
+export class Card {
+    constructor(public suite: string, public rank: string) {
+
+    }
+    toString(): string {
+        return "Card is " + this.rank + " of " + this.suite;
+    }
+}
