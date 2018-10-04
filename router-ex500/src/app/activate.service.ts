8d8ea9b38be60b44969ddd003b1e10be10a101fb
rds80 <rippleshah@msn.com>
2018-10-03 23:30:55 -0400

    Router Guard example

---
+++ router-ex500/src/app/activate.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { UserService } from './user.service';
+import { CanActivate } from '@angular/router';
+
+@Injectable()
+export class ActivateService implements CanActivate {
+
+  constructor(private _userService: UserService) { }
+  canActivate() {
+    return this._userService.authenticated;
+  }
+}
