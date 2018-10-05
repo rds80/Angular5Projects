731267dd37e4e25f747b29d4485fe447315c1270
rds80 <rippleshah@msn.com>
2018-10-05 18:41:39 -0400

    Path Parameters

--- http-ex300/src/app/app.component.ts
+++ http-ex300/src/app/app.component.ts
@@ -1,31 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <img width="300" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
     <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://github.com/angular/angular-cli/wiki">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
+      <li *ngFor="let post of _posts">
+        {{post.title}}&nbsp;&nbsp;<button (click)="showPost(post.id)">Show</button>
       </li>
     </ul>
-    
+    <div #modal id="myModal" class="modal">
+      <div class="modal-content">
+        <span class="close" (click)="closeModal()">&times;</span>
+        <h3>{{this._post.title}}</h3>
+        <p>{{this._post.body}}</p>
+      </div>
+    </div>  
   `,
   styles: []
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  _posts = [];
+  _post = {};
+@ViewChild('modal') _myModal: any;
+constructor(private _http: HttpClient) {
+
+}
+  
+ngOnInit() {
+  return this._http.get<any>("http://jsonplaceholder.typicode.com/posts")
+             .subscribe(
+                res => {this._posts = res;}
+             );
+  }
+
+showPost(postId: number) {
+  this._http.get<any>(`http://jsonplaceholder.typicode.com/posts/${postId}`)
+      .subscribe(
+        res => {
+          this._post = res;
+          this._myModal.nativeElement.style.display = 'block';
+        }
+      );
+}
+
+closeModal() {
+  this._myModal.nativeElement.style.display = 'none';
+}
+
 }
